0c034ba5172cc5f77f128631e3e110db4f85b583
Jakub Klewicki <kuba.klewicki@gmail.com>
2021-11-01 13:37:38 +0100

    Add filter for search
    1. Add filter for search by author and clear filter button
    2. Install cripsy_forms
    3. Add links icon for insta , tiktok, YT

--- src/apps/pages/filters.py
+++ src/apps/pages/filters.py
@@ -3,8 +3,8 @@ import django_filters
 from .models import * # gwiazdka importuje wszystkie modele
 
 class MovieFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(label='Wyszukaj film po nazwie:',lookup_expr='contains')  
+    author = django_filters.CharFilter(label='Wyszukaj film po nazwie influencera',lookup_expr='contains')  
     
     class Meta:
         model = Movie
-        fields = ['title']
+        fields = ['author']
