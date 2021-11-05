61f5f990a6f5999525c890210bf197a0bbae72a2
TomPrint <tomekklewicki@gmail.com>
2021-11-05 01:07:09 +0100

    New Bootstrap Navbar

---
+++ src/static/script.js
@@ -0,0 +1,17 @@
+
+
+
+// let logoBox = document.querySelector(".logo");
+// {/* <i class='bx bx-x'></i> */ }
+
+
+// logoBox.addEventListener("mouseover", () => {
+//     navbar.classList.toggle("showInput");
+//     if (navbar.classList.contains("showInput")) {
+//         logoBox.classList.replace("logo", "logo2")
+//     } else {
+
+//         logoBox.classList.replace("logo2", "logo")
+
+//     }
+// })
