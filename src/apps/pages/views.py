b54115756c789ff9fb3a17bde0f62450d561ec6c
TomPrint <tomekklewicki@gmail.com>
2021-11-09 00:21:53 +0100

    Added new navbar styling, and 'back to the top' button

--- src/apps/pages/views.py
+++ src/apps/pages/views.py
@@ -18,7 +18,7 @@ class MoviesView (AdminStaffRequiredMixin, FilterView):
     model=Movie
     template_name = 'pages/home.html'
     filterset_class = MovieFilter
-    paginate_by =  3
+    paginate_by = 10
 
     def get_queryset(self):
       qs = self.model.objects.all()
--- src/static/script.js
+++ src/static/script.js
@@ -1,3 +1,22 @@
+// BACK TO THE TOP BUTTON
+var btn = $('#button');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
+
+
+
+
 
 
 // let logoBox = document.querySelector(".logo");
