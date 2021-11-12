65fa443c705ff627076407720ab061637a334598
TomPrint <tomekklewicki@gmail.com>
2021-11-13 00:12:32 +0100

    Filter Search moved to Navbar

--- src/apps/pages/filters.py
+++ src/apps/pages/filters.py
@@ -1,9 +1,11 @@
+from typing import Text
 import django_filters
+from django.forms.widgets import TextInput
 
 from .models import * # gwiazdka importuje wszystkie modele
 
 class MovieFilter(django_filters.FilterSet):
-    author = django_filters.CharFilter(label='Wyszukaj film po nazwie influencera',lookup_expr='contains')  
+    author = django_filters.CharFilter(label='', lookup_expr='contains',  widget=TextInput(attrs={'placeholder': 'Szukaj'}))
     
     class Meta:
         model = Movie
