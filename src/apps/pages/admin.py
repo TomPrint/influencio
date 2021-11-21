2cb0ac5d96e7c95b24be68e3ee8a236971584387
Jakub Klewicki <kuba.klewicki@gmail.com>
2021-11-21 16:09:53 +0100

    Changes:
    1. Edit admin movie view
    2. Edit movie_model
    3. Edit movie views.py
    4. Changes in home.html
    5. Add new templates - movie,movie_tik_tok, movie_youtube, movie
    6. Changes in style.css

--- src/apps/pages/admin.py
+++ src/apps/pages/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Movie
 
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('author','title','category','source','promotion','date_posted')
 
 # Register your models here.
-admin.site.register(Movie)
+admin.site.register(Movie, MovieAdmin)
+
--- src/apps/pages/models.py
+++ src/apps/pages/models.py
@@ -6,24 +6,30 @@ from django.utils import timezone
 category_choices = (
     ('HOT-TOP', 'HOT-TOP'), ('ODKRYCIA', 'ODKRYCIA'), ('BEAUTY','BEAUTY'), ('ŚMIESZNE', 'ŚMIESZNE'),
     ('GAMING', 'GAMING'), ('LIFESTYLE', 'LIFESTYLE'), ('SZTUKA', 'SZTUKA'), ('SPORT','SPORT'),)
+
+source_choices = (
+    ('YouTube', 'YouTube'), ('TikTok', 'TikTok'), ('Instagram','Instagram'), ('Other', 'Other'), ('Promo', 'Promo'),)
 # Create your models here.
 
 class Movie (models.Model):
+    category = models.CharField(max_length=50, verbose_name='Kategoria filmu', default= 'unassigned', null=False, choices=category_choices)
+    source = models.CharField(max_length=50, verbose_name='Źródło filmu', default= 'unassigned', null=False, choices=source_choices)
+    promotion = models.BooleanField(default=False, verbose_name='PROMOCJA FILMU')
     author = models.CharField(max_length=50, verbose_name='Nazwa influencera')
     title = models.CharField(max_length=50, verbose_name='Nazwa filmu')
-    category = models.CharField(max_length=50, verbose_name='Kategoria filmu', default= 'unassigned', null=False, choices=category_choices)
     content = models.TextField(max_length=10000, verbose_name='HTML EMBEDED do filmu')
     date_posted = models.DateTimeField(default=timezone.now)
     youtube_url = models.URLField(blank=True, max_length=300)
     tiktok_url = models.URLField(blank=True, max_length=300)
     insta_url = models.URLField(blank=True, max_length=300)
+    
 
     class Meta:
         verbose_name = ("Movie")
         verbose_name_plural = ("Movies")
 
     def __str__(self):
-        return self.author +' - '+ self.title + " " "-"" " + self.category
+        return self.author +' - '+ self.title 
 
     def get_absolute_url(self):
         return reverse("_detail", kwargs={"pk": self.pk})
