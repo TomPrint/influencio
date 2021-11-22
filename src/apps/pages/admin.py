588dd57abdfc991127d06ab37b65bbef04bbbf0f
TomPrint <tomekklewicki@gmail.com>
2021-11-22 23:53:01 +0100

    Initial Setup for CSV upload from Admin page

--- src/apps/pages/admin.py
+++ src/apps/pages/admin.py
@@ -1,8 +1,39 @@
 from django.contrib import admin
+from django.urls import path
+from django.shortcuts import render
+from django import forms
 from .models import Movie
+
+class ImportCsvForm(forms.Form):
+    list_display = forms.FileField()
 
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('author','title','category','source','promotion','date_posted')
+    #admin filter
+    list_filter = ('author', 'title', )
+    #admin search
+    search_fields = ['author', 'title',]
+
+    def get_urls(self):
+        urls = super().get_urls()
+        new_urls = [
+            path('upload-csv/', self.upload_csv),
+            ]
+        return new_urls + urls
+
+    def upload_csv(self, request):
+        #test for post from form
+        if request.method == 'POST':
+            print("action is post")
+
+
+        form = ImportCsvForm()
+        data = {"form": form}
+        return render(request, 'admin/csv_upload.html', data)
+
+
+
+
 
 # Register your models here.
 admin.site.register(Movie, MovieAdmin)
