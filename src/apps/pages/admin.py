cfffe8e099c14ecb9e753ca0a519186a2ec41f65
TomPrint <tomekklewicki@gmail.com>
2021-11-24 00:00:58 +0100

    Added CSV download from database through admin page

--- src/apps/pages/admin.py
+++ src/apps/pages/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
+from django.http.response import HttpResponse
 from django.urls import path
 from django.shortcuts import render
 from django import forms
+
 from .models import Movie
+import csv
 
 class ImportCsvForm(forms.Form):
     list_display = forms.FileField()
+
 
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('author','title','category','source','promotion','date_posted')
@@ -14,17 +18,45 @@ class MovieAdmin(admin.ModelAdmin):
     #admin search
     search_fields = ['author', 'title',]
 
+    #urls for CSV dupload & download
     def get_urls(self):
         urls = super().get_urls()
         new_urls = [
             path('upload-csv/', self.upload_csv),
+            path('download-csv/', self.download_csv),
+            path('download-csv/export', self.export),
             ]
         return new_urls + urls
 
+    #CSV Querry
+    def export(request, self):
+        response = HttpResponse(content_type='text/csv')
+        writer = csv.writer(response)
+        writer.writerow (['Category', 'Source ','Promotion', 'Author', 'Title', 'Content', 'Date Posted', 'Youtube URL', 'Tiktok URL', 'Insta URL'])
+
+        for movie in Movie.objects.all().values_list('category', 'source','promotion', 'author', 'title', 'content', 'date_posted', 'youtube_url', 'tiktok_url', 'insta_url'):
+            writer.writerow(movie)
+        
+        response['Content-Disposition'] = 'attachement; filename="movies.csv"'
+      
+        return response
+
+
+    #views for CSV download page  
+    def download_csv(self, request):
+        return render(request, 'admin/csv_download.html')
+
+
+    #views for CSV upload page
     def upload_csv(self, request):
-        #test for post from form
         if request.method == 'POST':
-            print("action is post")
+            csv_file = request.FILES["csv_upload"]
+
+            file_data = csv_file.read().decode('utf-8')
+            csv_data = file_data.split("\n")
+            
+            for x in csv_data:
+                fields = x.split(";")
 
 
         form = ImportCsvForm()
