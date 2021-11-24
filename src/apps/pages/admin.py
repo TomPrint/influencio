7cc02106c0e781def69cd27d0976d4195ed349e7
TomPrint <tomekklewicki@gmail.com>
2021-11-24 23:28:30 +0100

    Custom admin URLs login authorized

--- src/apps/pages/admin.py
+++ src/apps/pages/admin.py
@@ -3,8 +3,11 @@ from django.contrib.admin.decorators import action
 from django.http.response import HttpResponse
 from django.urls import path
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django import forms
 from django.utils.translation import ngettext
+
+
 from .models import Movie
 import csv
 
@@ -33,39 +36,50 @@ class MovieAdmin(admin.ModelAdmin):
         return new_urls + urls
 
     #CSV Querry
-    def export(request, self):
-        response = HttpResponse(content_type='text/csv')
-        writer = csv.writer(response)
-        writer.writerow (['Category', 'Source ','Promotion', 'Author', 'Title', 'Content', 'Date Posted', 'Youtube URL', 'Tiktok URL', 'Insta URL'])
+    def export(self, request):
+        if request.user.is_staff:
 
-        for movie in Movie.objects.all().values_list('category', 'source','promotion', 'author', 'title', 'content', 'date_posted', 'youtube_url', 'tiktok_url', 'insta_url'):
-            writer.writerow(movie)
+            response = HttpResponse(content_type='text/csv')
+            writer = csv.writer(response)
+            writer.writerow (['Category', 'Source ','Promotion', 'Author', 'Title', 'Content', 'Date Posted', 'Youtube URL', 'Tiktok URL', 'Insta URL'])
+
+            for movie in Movie.objects.all().values_list('category', 'source','promotion', 'author', 'title', 'content', 'date_posted', 'youtube_url', 'tiktok_url', 'insta_url'):
+                writer.writerow(movie)
         
-        response['Content-Disposition'] = 'attachement; filename="movies.csv"'
+            response['Content-Disposition'] = 'attachement; filename="movies.csv"'
       
-        return response
+            return response
+        else:
+            return redirect('page-home')
 
 
     #views for CSV download page  
     def download_csv(self, request):
-        return render(request, 'admin/csv_download.html')
+        if request.user.is_authenticated:
+            return render(request, 'admin/csv_download.html')
+        else:
+            return redirect('page-home')
 
 
     #views for CSV upload page
     def upload_csv(self, request):
-        if request.method == 'POST':
-            csv_file = request.FILES["csv_upload"]
+        if request.user.is_authenticated:
+            if request.method == 'POST':
+                csv_file = request.FILES["csv_upload"]
 
-            file_data = csv_file.read().decode('utf-8')
-            csv_data = file_data.split("\n")
+                file_data = csv_file.read().decode('utf-8')
+                csv_data = file_data.split("\n")
             
-            for x in csv_data:
-                fields = x.split(";")
+                for x in csv_data:
+                    fields = x.split(";")
 
 
-        form = ImportCsvForm()
-        data = {"form": form}
-        return render(request, 'admin/csv_upload.html', data)
+            form = ImportCsvForm()
+            data = {"form": form}
+            return render(request, 'admin/csv_upload.html', data)
+
+        else:
+            return redirect('page-home')
 
     @admin.action(description='Dodaj do promowanych')
     def make_promotion(self, request, queryset):
