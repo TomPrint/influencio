164189f9f6419920643e6c427021a849cf981003
Jakub Klewicki <kuba.klewicki@gmail.com>
2021-12-14 23:03:31 +0100

    Edit contact form:
    1. Edit def contact in views.py
    2. Edit contact.html
    3. Edit Settings.py - contact form settings
    4. Edit base.html - add messages section div

--- src/influencio_app/settings.py
+++ src/influencio_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +137,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#CONTACT FORM EMAIL SETTINGS FOR DEVELOPMENT
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'influencio.kontakt@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_KEY_2')
+
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
